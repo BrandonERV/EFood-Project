8f537c0254f03473b399a42ef6aba6993764a516
sigi24 <sjchaconballadares@gmail.com>
2023-08-27 11:03:04 -0600

    Prueba commit Sigifredo

--- WebE-Food/WebE-Food/Models/ErrorViewModel.cs
+++ WebE-Food/WebE-Food/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace WebE_Food.Models
     public class ErrorViewModel
     {
         //Mariano
+        //Sigifredo
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
