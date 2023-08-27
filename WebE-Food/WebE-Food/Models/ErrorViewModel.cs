e360916077f37cea66b9eb08b118ae22207e0399
M4R14N0CR <mariano21803@gmail.com>
2023-08-26 22:02:17 -0600

    Prueba commit mariano

--- WebE-Food/WebE-Food/Models/ErrorViewModel.cs
+++ WebE-Food/WebE-Food/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace WebE_Food.Models
 {
     public class ErrorViewModel
     {
+        //Mariano
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
